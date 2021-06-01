d31b41f441751e6a168ba2dfffb5bd3452c61e80
l4kk3r <66743739+l4kk3r@users.noreply.github.com>
2021-06-01 12:40:02 +0300

    Add files via upload

--- swagger_connection.js
+++ swagger_connection.js
@@ -13,7 +13,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:8080"
+                url: "https://test-rest8.herokuapp.com"
             }
         ],
     },
